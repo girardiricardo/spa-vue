8e7c67334ee88e5bcea0406d17d26556142f81c4
Ricardo Girardi <girardiricardo@outlook.com>
2018-08-19 20:27:37 -0300

    Creating vue components

---
+++ src/components/Signin.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-container fluid>
+        <v-layout column>
+            <v-flex xs12 class="text-xs-center" mt-5>
+                <h1>Signin Page</h1>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
