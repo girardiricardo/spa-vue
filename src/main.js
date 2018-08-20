f8e18f4c47791c02a582419cb00ff70afe27a994
Ricardo Girardi <girardiricardo@outlook.com>
2018-08-19 22:18:21 -0300

    Importing store file

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App'
 import router from './router'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
+import { store } from './store'
 
 Vue.use(Vuetify)
 
@@ -12,5 +13,6 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
+  store,
   render: h => h(App)
 })
