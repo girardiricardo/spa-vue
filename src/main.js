646e65a5c92eba114d859ba850251df9aea32b3e
Ricardo Girardi <girardiricardo@outlook.com>
2018-08-19 22:59:31 -0300

    Setting up firebase

--- src/main.js
+++ src/main.js
@@ -4,8 +4,16 @@ import router from './router'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 import { store } from './store'
+import firebase from 'firebase'
 
 Vue.use(Vuetify)
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyB52BNYe4un14blQWrMV6qe078zamYEVFs',
+  authDomain: 'vue-spa-5c885.firebaseapp.com',
+  databaseURL: 'https://vue-spa-5c885.firebaseio.com',
+  projectId: 'vue-spa-5c885'
+})
 
 Vue.config.productionTip = false
 
